59ce10d646f15113baacf8cdfe516d46cbe180e1
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2026-02-03 12:09:34 +0300

    Time: 3 ms (88.65%) | Memory: 19.3 MB (27.78%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        y = str(x)
+        l = 0
+        r = len(y) -1
+        while l <= r:
+            if y[l] !=  y[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
