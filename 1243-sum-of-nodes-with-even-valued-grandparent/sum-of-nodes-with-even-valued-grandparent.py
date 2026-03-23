e465edffbdb3e9d5d6ba819869f4ee9ddc1e3367
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2026-03-23 19:41:34 +0300

    Time: 15 ms (13.55%) | Memory: 22.2 MB (73.34%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.py
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.py
@@ -6,17 +6,21 @@
 #         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        q = deque([(root, -1, -1)])
-        val = 0
-        while q:
-            node, parent, grandP = q.popleft()
-            if grandP % 2 == 0:
-                val += node.val 
-            if node.left:
-                q.append([node.left, node.val, parent])
-            if node.right:
-                q.append([node.right, node.val, parent])
-        return val
+        ans =[0]
+        def help(node, ans):
+            if not node:
+                return 
+            if node.val % 2==0:
+                if node.left and node.left.left:
+                    ans[0] += node.left.left.val
+                if node.left and node.left.right:
+                    ans[0] += node.left.right.val
+                if node.right and node.right.right:
+                    ans[0] += node.right.right.val
+                if node.right and node.right.left:
+                    ans[0] += node.right.left.val
 
+            help(node.left, ans)
+            help(node.right, ans)
+        help(root, ans)
+        return ans[0]
