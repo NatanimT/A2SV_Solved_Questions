d7c08ee88093a675bcb146dd342de632bb05f138
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2026-04-19 02:19:34 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.11%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        count = [0, 0 ,0 ]
+        for i in nums:
+            count[i] += 1
+        r, w, b = count
+        nums[:r] = [0] * r
+        nums[r:r+w] = [1] * w
+        nums[r+w:] = [2] * b
+        
+
+        
